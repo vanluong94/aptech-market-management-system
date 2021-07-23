7bc809a880d370731fb2f4222146b3fd4ad2105f
VanLuong <van.luong.94114@gmail.com>
2021-07-23 14:03:20 +0700

    [update] JDBC connection to remote server

--- src/main/java/vn/aptech/quanlybanhang/Main.java
+++ src/main/java/vn/aptech/quanlybanhang/Main.java
@@ -5,20 +5,41 @@
  */
 package vn.aptech.quanlybanhang;
 
+import java.sql.SQLException;
 import vn.aptech.quanlybanhang.menu.MainMenu;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import vn.aptech.quanlybanhang.utilities.DBConnection;
 
 /**
  *
  * @author vanluong
  */
 public class Main {
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.start(scanner);
+        try {
+            
+            /* App start */
+            
+            DBConnection.getConnection();
+            DBConnection.showTables();
+//            Scanner scanner = new Scanner(System.in);
+//            MainMenu mainMenu = new MainMenu();
+//            mainMenu.start(scanner);
+            
+            DBConnection.closeConnection(); // connection should be closed to avoid database connection sleeps on server.
+            /* App close */
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
+
