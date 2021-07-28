4877d4b17b9c92e44fcc038992e2a4207f25c7ae
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-07-28 07:53:42 +0700

    [Add] ProductDAO & ProductService

---
+++ src/main/java/vn/aptech/quanlybanhang/dao/ProductDAO.java
@@ -0,0 +1,14 @@
+/*
+ * Do an Java tai HaNoi Aptech
+ */
+package vn.aptech.quanlybanhang.dao;
+
+import vn.aptech.quanlybanhang.entities.Product;
+
+/**
+ *
+ * @author Nguyen Ba Tuan Anh <anhnbt.it@gmail.com>
+ */
+public interface ProductDAO extends BaseDAO<Product> {
+    
+}
---
+++ src/main/java/vn/aptech/quanlybanhang/service/ProductService.java
@@ -0,0 +1,14 @@
+/*
+ * Do an Java tai HaNoi Aptech
+ */
+package vn.aptech.quanlybanhang.service;
+
+import vn.aptech.quanlybanhang.entities.Product;
+
+/**
+ *
+ * @author Nguyen Ba Tuan Anh <anhnbt.it@gmail.com>
+ */
+public interface ProductService extends BaseService<Product> {
+    
+}
