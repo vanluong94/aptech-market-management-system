f1b882b7cbb42b667226359b26a875f4d71732ae
vanluong94 <van.luong.94114@gmail.com>
2021-07-29 01:09:19 +0000

    add ProductService

---
+++ src/main/java/vn/aptech/quanlybanhang/dao/ProductDAOImpl.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.aptech.quanlybanhang.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import vn.aptech.quanlybanhang.entities.Product;
+
+
+public class ProductDAOImpl implements ProductDAO {
+
+    @Override
+    public boolean create(Product object) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Product object) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteById(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Product findById(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Product> findAll() throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
