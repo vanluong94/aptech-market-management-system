f70e006a611dada8be154f13fe86bb32d7de89b5
Vũ Long <vuduylong1999@gmail.com>
2021-08-04 09:56:19 +0000

    [feat] add Menu create Product

--- src/main/java/vn/aptech/quanlybanhang/pages/ProductCreatePage.java
+++ src/main/java/vn/aptech/quanlybanhang/pages/ProductCreatePage.java
@@ -5,17 +5,57 @@
  */
 package vn.aptech.quanlybanhang.pages;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import vn.aptech.quanlybanhang.entities.Brand;
+import vn.aptech.quanlybanhang.entities.Category;
+import vn.aptech.quanlybanhang.entities.Employee;
+import vn.aptech.quanlybanhang.entities.Product;
+import vn.aptech.quanlybanhang.service.AuthService;
+import vn.aptech.quanlybanhang.service.AuthServiceImpl;
+import vn.aptech.quanlybanhang.service.ProductService;
+import vn.aptech.quanlybanhang.service.ProductServiceImpl;
+import vn.aptech.quanlybanhang.utilities.AppScanner;
 
 public class ProductCreatePage extends Page {
 
     @Override
     public void displayContent() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ProductService productService = new ProductServiceImpl();
+        Brand brandId = new Brand();
+        brandId.setBrandId(AppScanner.scanIntWithMessage("Nhap Brand ID : "));
+
+        Category categoryId = new Category();
+        categoryId.setCategoryId(AppScanner.scanIntWithMessage("Nhap Category ID :"));
+
+//        Employee employeeId = new Employee();
+//        employeeId.getEmployeeId();
+
+//        AuthService employeeId2 = new AuthServiceImpl();
+//        employeeId2.getCurrentEmployee();
+        
+        
+        String productName = AppScanner.scanStringWithMessage("Nhap ten San Pham :");
+        double productPrice = AppScanner.scanDoubleWithMessage("Gia tien :");
+        int productStock = AppScanner.scanIntWithMessage("So luong :");
+
+        Product productNew = new Product(brandId, categoryId, AuthServiceImpl.getCurrentEmployee(), productName, productPrice, productStock);
+
+        try {
+            if (productService.create(productNew)) {
+                System.out.println("Them San Pham moi thanh cong!");
+            } else {
+                System.out.println("Da xay ra loi");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(ProductCreatePage.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public String getTitle() {
         return "Tao San Pham";
     }
-    
+
+
 }
--- src/main/java/vn/aptech/quanlybanhang/pages/ProductDeletePage.java
+++ src/main/java/vn/aptech/quanlybanhang/pages/ProductDeletePage.java
@@ -5,17 +5,36 @@
  */
 package vn.aptech.quanlybanhang.pages;
 
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import vn.aptech.quanlybanhang.service.ProductService;
+import vn.aptech.quanlybanhang.service.ProductServiceImpl;
+import vn.aptech.quanlybanhang.service.SupplierServiceImpl;
 
 public class ProductDeletePage extends Page {
 
     @Override
     public void displayContent() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            ProductService productService = new ProductServiceImpl();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Nhap ID san pham muon xoa : ");
+            int productId = sc.nextInt();
+            if (productService.deleteById(productId)) {
+                System.out.println("Xoa San pham thanh cong!");
+            }else{
+                System.out.println("Da xay ra loi");
+            }
+        } catch (Exception e) {
+              Logger.getLogger(ProductServiceImpl.class.getName()).log(Level.SEVERE, null, e);
+        }
+
     }
 
     @Override
     public String getTitle() {
         return "Xoa San Pham";
     }
-    
+
 }
