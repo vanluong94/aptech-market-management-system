1cdad1cd3911c8d4c0d875d40d372a1849f457e5
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-08-09 14:45:03 +0000

    [feat] update profile

--- src/main/java/vn/aptech/quanlybanhang/pages/PageUpdateProfile.java
+++ src/main/java/vn/aptech/quanlybanhang/pages/PageUpdateProfile.java
@@ -1,21 +1,55 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Do an Java tai HaNoi Aptech
  */
 package vn.aptech.quanlybanhang.pages;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import vn.aptech.quanlybanhang.entities.Employee;
+import vn.aptech.quanlybanhang.service.AuthServiceImpl;
+import vn.aptech.quanlybanhang.service.EmployeeService;
+import vn.aptech.quanlybanhang.service.EmployeeServiceImpl;
+import vn.aptech.quanlybanhang.utilities.AppScanner;
 
+/**
+ *
+ * @author Nguyen Ba Tuan Anh <anhnbt.it@gmail.com>
+ */
 public class PageUpdateProfile extends Page {
+    
+    private final EmployeeService empService;
 
+    public PageUpdateProfile() {
+        this.empService = new EmployeeServiceImpl();
+    }
+    
     @Override
     public void displayContent() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Employee emp = AuthServiceImpl.getCurrentEmployee();
+            System.out.println("ID: " + emp.getEmployeeId());
+            System.out.println("Tài khoản: " + emp.getUserName());
+            System.out.println("Chức vụ: " + emp.getDepartment());
+            String name = AppScanner.scanStringWithMessage("Họ tên: ", false);
+            AppScanner.getScanner().nextLine();
+            String address = AppScanner.scanStringWithMessage("Địa chỉ: ", false);
+            String phone = AppScanner.scanStringWithMessage("Số điện thoại: ", false);
+            emp.setName(name);
+            emp.setAddress(address);
+            emp.setPhone(phone);
+            if (empService.update(emp)) {
+                System.out.println("Cập nhật thành công");
+            } else {
+                System.out.println("Đã xảy ra lỗi");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(PageUpdateProfile.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public String getTitle() {
-        return "Cap nhat thong tin";
+        return "Cập nhật thông tin";
     }
     
 }
