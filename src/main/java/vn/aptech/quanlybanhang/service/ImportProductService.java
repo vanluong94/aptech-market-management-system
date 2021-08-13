24c7876171c805d623eaaad98d2cc72a7f80890d
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-08-13 00:56:56 +0000

    [feat] import product

---
+++ src/main/java/vn/aptech/quanlybanhang/service/ImportProductService.java
@@ -0,0 +1,14 @@
+/*
+ * Do an Java tai HaNoi Aptech
+ */
+package vn.aptech.quanlybanhang.service;
+
+import vn.aptech.quanlybanhang.entities.ImportProduct;
+
+/**
+ *
+ * @author Nguyen Ba Tuan Anh <anhnbt.it@gmail.com>
+ */
+public interface ImportProductService extends BaseService<ImportProduct> {
+    
+}
